1042a002b5189371b2186e32c1aa1edb86427da4
Gmes23 <gmesda23@gmail.com>
2017-02-22 19:15:01 -0500

    finish right container layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import LeftSideBar from './components/left_side_bar';
 import HeaderNavbar from './components/header_navbar';
+import RightContainer from './components/right_container';
+
 
 import './App.css';
 
@@ -10,6 +12,7 @@ class App extends Component {
       <div className="App">
         <LeftSideBar />
         <HeaderNavbar />
+        <RightContainer />
      </div>
     );
   }
---
+++ src/components/right_container.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import '../../public/style/right_container.css';
+
+class RightContainer extends Component {
+  render() {
+    return (
+      <div className="right_container_main">
+          <div className="right_container_header-links">
+            <ul className="NoDecoration">
+                <li> Results </li>
+                <li> Results </li>
+                <li> Results </li>
+                <li> Results </li>
+            </ul>
+          </div>
+          <div className="feature-container">
+            <div className="text-current-feature">
+                <p> Up Next </p>
+            </div>
+            <div className="add-continuously-playlist">
+                <button> autoadd </button>
+            </div>
+          </div>
+
+          <div className="video-render-list-container">
+            <div className="video-render-list-items">
+                <div className="video-render-list-thumbnail">
+                </div>
+                <div className="video-render-list-details">
+                    <p> Title of Song </p>
+                    <br />
+                    <h6> Artist Name ft Artist </h6>
+                </div>
+                <div className="video-render-list-options">
+                </div>
+            </div>
+
+             <div className="video-render-list-items">
+                <div className="video-render-list-thumbnail">
+                </div>
+                <div className="video-render-list-details">
+                    <p> Title of Song </p>
+                    <br />
+                    <h6> Artist Name ft Artist </h6>
+                </div>
+                <div className="video-render-list-options">
+                </div>
+            </div>
+
+             <div className="video-render-list-items">
+                <div className="video-render-list-thumbnail">
+                </div>
+                <div className="video-render-list-details">
+                    <p> Title of Song </p>
+                    <br />
+                    <h6> Artist Name ft Artist </h6>
+                </div>
+                <div className="video-render-list-options">
+                </div>
+            </div>
+
+             <div className="video-render-list-items">
+                <div className="video-render-list-thumbnail">
+                </div>
+                <div className="video-render-list-details">
+                    <p> Title of Song </p>
+                    <br />
+                    <h6> Artist Name ft Artist </h6>
+                </div>
+                <div className="video-render-list-options">
+                </div>
+            </div>
+
+             <div className="video-render-list-items">
+                <div className="video-render-list-thumbnail">
+                </div>
+                <div className="video-render-list-details">
+                    <p> Title of Song </p>
+                    <br />
+                    <h6> Artist Name ft Artist </h6>
+                </div>
+                <div className="video-render-list-options">
+                </div>
+            </div>
+
+            
+
+          </div>
+
+      </div>
+    );
+  }
+}
+
+export default RightContainer;
